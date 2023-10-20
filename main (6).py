590d28ca98edd07fa4310f0472abccba4ee76074
Magieba <144255138+Magieba@users.noreply.github.com>
2023-10-20 21:27:01 +0530

    Update main (6).py

--- main (6).py
+++ main (6).py
@@ -1,29 +1,20 @@
-def CgpaCalc(marks, n):
- 
-  
-    grade = [0] * n
-   
-     
-    
-    Sum = 0
-   
-    for i in range(n):
-       grade[i] = (marks[i] / 10)
-   
-    
-    for i in range(n):
-       Sum += grade[i]
-   
-    
-    cgpa = Sum / n
-   
-    return cgpa
-   
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_student;
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-n = 5
-marks = [ 90, 80, 70, 80, 90 ]
- 
-cgpa = CgpaCalc(marks, n)
-       
-print("CGPA = ", '%.1f' % cgpa)
-print("CGPA Percentage = ", '%.2f' % (cgpa * 9.5))
+students = [
+    Student("Alice", "A123", 3.8),
+    Student("Bob", "B456", 3.5),
+    Student("Charlie", "C789", 3.9),
+    Student("David", "D234", 3.7),
+]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
